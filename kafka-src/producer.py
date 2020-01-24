a3658725bd79da6f8b9757b2638b8c1e8175ffae
matthewlamdotjs <matthewlam.js@gmail.com>
2020-01-24 10:23:49 -0800

    string concat issure

--- kafka-src/producer.py
+++ kafka-src/producer.py
@@ -6,5 +6,5 @@ producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
 counter = 0
 
 while(True):
-    producer.send('test', b'message '+ counter)
+    producer.send('test', b'message '+ str(counter))
     counter = counter + 1
