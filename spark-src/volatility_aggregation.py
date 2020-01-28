57496c74b32dec86e4f2c8552fde55c5d35eb534
matthewlamdotjs <matthewlam.js@gmail.com>
2020-01-28 12:33:02 -0800

    write agg df

--- spark-src/volatility_aggregation.py
+++ spark-src/volatility_aggregation.py
@@ -9,6 +9,7 @@ try:
     DB_PORT = os.environ['CJ_DB_PORT']
     DB_USER = os.environ['CJ_DB_UN']
     DB_PASS = os.environ['CJ_DB_PW']
+    DRIVER_PATH = os.environ['PG_JDBC_DRIVER']
 except:
     print('Missing credentials. Please set environment variables appropriately.')
     exit()
@@ -17,7 +18,7 @@ except:
 spark = SparkSession.builder \
     .master('local') \
     .appName('volatility_aggregation') \
-    .config('spark.jars', '/usr/local/postgresql-42.2.9.jar') \
+    .config('spark.jars', DRIVER_PATH) \
     .getOrCreate()
     
 # read in data
@@ -34,26 +35,38 @@ rawDF = spark.read \
 rawDF.createOrReplaceTempView('prices')
 
 # define moving window frame
-start_date = (date.today() - timedelta(days=1)).strftime('%Y-%m-%d')
-end_date = (date.today() - timedelta(days=15)).strftime('%Y-%m-%d')
+start_date = (date.today() - timedelta(days=15)).strftime('%Y-%m-%d')
+end_date = (date.today() - timedelta(days=1)).strftime('%Y-%m-%d')
 
 # aggregate data using std dev and mean for each window
 sqlDF = spark.sql("""
     SELECT
         symbol,
-        '"""+ start_date +"""' AS start_date,
-        '"""+ end_date +"""' AS end_date,
-        stddev(price_close) AS price_deviation,
-        avg(price_close) AS average_price
+        cast('"""+ start_date +"""' as date) AS start_date,
+        cast('"""+ end_date +"""' as date) AS end_date,
+        cast(stddev(price_close) as decimal(8,4)) AS price_deviation,
+        cast(avg(price_close) as decimal(8,4)) AS average_price
     FROM
-        daily_prices_temp_tbl
+        prices
     WHERE
-        CAST(date AS INT)
-        BETWEEN unix_timestamp('"""+ start_date +"""', 'yyyy-MM-dd HH:mm:ss')
-        AND unix_timestamp('"""+ end_date +"""', 'yyyy-MM-dd HH:mm:ss')
+        date >= cast('"""+ start_date +"""' as date) AND
+        date < cast('"""+ end_date +"""' as date)
     GROUP BY
         symbol
     ORDER BY
-        symbol;
+        symbol
 """)
 
+sqlDF.show()
+
+sqlDF.write.mode('append') \
+    .format('jdbc') \
+    .option('url', 'jdbc:postgresql://'+DB_URL+':'+DB_PORT+'/postgres') \
+    .option('dbtable', 'volatility_aggregation_tbl') \
+    .option('user', DB_USER) \
+    .option('password', DB_PASS) \
+    .option('driver', 'org.postgresql.Driver') \
+    .save()
+
+# end session
+spark.stop()
