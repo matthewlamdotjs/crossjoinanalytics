92f1f22d6daeeb6e38ced5015c9c9871c154d33f
matthewlamdotjs <matthewlam.js@gmail.com>
2020-01-28 10:59:07 -0800

    change script to work from any dir

--- spark-src/submit.sh
+++ spark-src/submit.sh
@@ -3,5 +3,5 @@
 /usr/local/spark/bin/spark-submit \
   --driver-class-path /usr/local/postgresql-42.2.9.jar \
   --master spark://localhost:7077 \
-  volatility_aggregation.py \
+  ~/crossjoinanalytics/spark-src/volatility_aggregation.py \
   1000
