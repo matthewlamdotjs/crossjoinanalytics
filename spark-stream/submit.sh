6c501ddf591030e1feb935fcaefdf903e31f949f
matthewlamdotjs <matthewlam.js@gmail.com>
2020-01-30 15:33:50 -0800

    include jars

--- spark-stream/submit.sh
+++ spark-stream/submit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 /usr/local/spark/bin/spark-submit \
+  --packages org.apache.spark:spark-streaming-kafka-0-8:2.4.0 \
   --driver-class-path /usr/local/postgresql-42.2.9.jar \
   --master spark://localhost:7077 \
   ~/crossjoinanalytics/spark-stream/stream-consumer.py \
