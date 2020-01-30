2d6f50b70d3945cbe5803b4d4b0d01857096701e
matthewlamdotjs <matthewlam.js@gmail.com>
2020-01-30 15:37:34 -0800

    change submit script to include multi jar

--- spark-stream/submit.sh
+++ spark-stream/submit.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 /usr/local/spark/bin/spark-submit \
-  --packages org.apache.spark:spark-streaming-kafka-0-8:2.4.0 \
-  --driver-class-path /usr/local/postgresql-42.2.9.jar \
+  --driver-class-path /usr/local/postgresql-42.2.9.jar,~/spark-streaming-kafka-0-10-assembly_2.10-2.2.3.jar \
   --master spark://localhost:7077 \
   ~/crossjoinanalytics/spark-stream/stream-consumer.py \
   1000
