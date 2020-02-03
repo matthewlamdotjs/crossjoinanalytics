08f2d30adcac381dcb58ebc7180b1ceabf53407d
matthewlamdotjs <matthewlam.js@gmail.com>
2020-02-03 14:50:21 -0800

    clean structure

--- spark-batch/submit.sh
+++ spark-batch/submit.sh
@@ -3,6 +3,6 @@
 /usr/local/spark/bin/spark-submit \
   --driver-class-path /usr/local/postgresql-42.2.9.jar \
   --master spark://localhost:7077 \
-  ~/crossjoinanalytics/spark-src/volatility_aggregation.py \
+  ~/crossjoinanalytics/spark-batch/volatility_aggregation.py \
   1000
   
