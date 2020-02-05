0fae2353fb26a80fe417372c62f74473b82f3540
matthewlamdotjs <matthewlam.js@gmail.com>
2020-02-05 09:23:34 -0800

    add historical job submit

---
+++ spark-batch/submit_historical.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+/usr/local/spark/bin/spark-submit \
+  --driver-class-path /usr/local/postgresql-42.2.9.jar \
+  --master spark://localhost:7077 \
+  ~/crossjoinanalytics/spark-batch/historical_aggregation.py \
+  1000
+  
