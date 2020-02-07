dd27adc51a90c93f1740815db71200c934ca2714
matthewlamdotjs <matthewlam.js@gmail.com>
2020-02-07 12:34:22 -0800

    try rdd partition w/o db write test

--- spark-stream/stream-consumer.py
+++ spark-stream/stream-consumer.py
@@ -29,98 +29,104 @@ ssc = StreamingContext(sc, 0.5)
 directKafkaStream = KafkaUtils.createDirectStream(ssc, ['stock-prices'],
     {'bootstrap.servers': SERVERS})
 
+# create individual message process function
+def processMessage(record):
+    # get message value
+    response = record[1]
+
+    try:
+        # parse json
+        js_payload = json.loads(response)
+        symbol = js_payload['Meta Data']['2. Symbol']
+        ts = js_payload['Time Series (Daily)']
+
+        # aggregate response into a dataframe-convertable format
+        def normalize(row):
+            nested = row[1]
+            return [symbol, row[0], nested['2. high'], nested['3. low'], nested['1. open'], nested['4. close']]
+        
+        thelist = list(
+            map(normalize, list(map(list, ts.items())))
+        )
+
+        # Connect DB
+        connection = psycopg2.connect(user = DB_USER,
+                                        password = DB_PASS,
+                                        host = DB_URL,
+                                        port = DB_PORT,
+                                        database = 'postgres')
+        connection.autocommit = True
+
+        cursor = connection.cursor()
+
+        # Get symbol currency
+        cursor.execute(
+            'SELECT currency FROM symbol_master_tbl where symbol = \''+symbol+'\';'
+        )
+        currency = cursor.fetchone()[0]
+
+        # create currency converter
+        c = CurrencyConverter() # c.convert(x, y, 'USD')
+        def convert_usd(price_close):
+            return c.convert(price_close, currency, 'USD')
+        
+        # get exisiting data
+        datesDF = psql.read_sql("""
+            SELECT
+                date
+            FROM
+                daily_prices_temp_tbl
+            WHERE
+                symbol = '"""+symbol+"""';
+        """, connection)
+
+        # make DF from new data
+        newDF = pd.DataFrame(thelist, columns =['symbol','date','price_high','price_low','price_open','price_close'])
+
+        # subtract old data
+        key_diff = set(newDF.date).difference(datesDF.date)
+        where_diff = newDF.date.isin(key_diff)
+
+        to_insert = newDF[where_diff]
+        to_insert['price_usd'] = to_insert['price_close'].apply(convert_usd)
+
+        print(currency)
+        print(to_insert.head())
+
+        # write to postgres
+        # pandas.io.sql patched function
+        # https://gist.github.com/jorisvandenbossche/10841234
+        def _write_postgresql(frame, table, names, cur):
+            bracketed_names = ['"' + column + '"' for column in names]
+            col_names = ','.join(bracketed_names)
+            wildcards = ','.join([r'%s'] * len(names))
+            insert_query = 'INSERT INTO public.%s (%s) VALUES (%s)' % (
+                table, col_names, wildcards)
+            data = [tuple(x) for x in frame.values]
+            print insert_query
+            print data
+            cur.executemany(insert_query, data)
+
+        # _write_postgresql(
+        #     to_insert,
+        #     'daily_prices_temp_tbl',
+        #     [s.replace(' ', '_').strip() for s in to_insert.columns],
+        #     cursor
+        # )
+
+    except (Exception) as error :
+        print('PySparkError: ' + str(error))
+
+
 # function to apply to each streamed item
 def processStream(time, rdd):
 
-    def rddProcess(record):
+    def rddProcess(partition):
 
-        # get message value
-        response = record[1]
-    
-        try:
-            # parse json
-            js_payload = json.loads(response)
-            symbol = js_payload['Meta Data']['2. Symbol']
-            ts = js_payload['Time Series (Daily)']
+        partition.foreach(processMessage)
 
-            # aggregate response into a dataframe-convertable format
-            def normalize(row):
-                nested = row[1]
-                return [symbol, row[0], nested['2. high'], nested['3. low'], nested['1. open'], nested['4. close']]
-            
-            thelist = list(
-                map(normalize, list(map(list, ts.items())))
-            )
 
-            # Connect DB
-            connection = psycopg2.connect(user = DB_USER,
-                                            password = DB_PASS,
-                                            host = DB_URL,
-                                            port = DB_PORT,
-                                            database = 'postgres')
-            connection.autocommit = True
-
-            cursor = connection.cursor()
-
-            # Get symbol currency
-            cursor.execute(
-                'SELECT currency FROM symbol_master_tbl where symbol = \''+symbol+'\';'
-            )
-            currency = cursor.fetchone()[0]
-
-            # create currency converter
-            c = CurrencyConverter() # c.convert(x, y, 'USD')
-            def convert_usd(price_close):
-                return c.convert(price_close, currency, 'USD')
-            
-            # get exisiting data
-            datesDF = psql.read_sql("""
-                SELECT
-                    date
-                FROM
-                    daily_prices_temp_tbl
-                WHERE
-                    symbol = '"""+symbol+"""';
-            """, connection)
-
-            # make DF from new data
-            newDF = pd.DataFrame(thelist, columns =['symbol','date','price_high','price_low','price_open','price_close'])
-
-            # subtract old data
-            key_diff = set(newDF.date).difference(datesDF.date)
-            where_diff = newDF.date.isin(key_diff)
-
-            to_insert = newDF[where_diff]
-            to_insert['price_usd'] = to_insert['price_close'].apply(convert_usd)
-
-            print(currency)
-            print(to_insert.head())
-
-            # write to postgres
-            # pandas.io.sql patched function
-            # https://gist.github.com/jorisvandenbossche/10841234
-            def _write_postgresql(frame, table, names, cur):
-                bracketed_names = ['"' + column + '"' for column in names]
-                col_names = ','.join(bracketed_names)
-                wildcards = ','.join([r'%s'] * len(names))
-                insert_query = 'INSERT INTO public.%s (%s) VALUES (%s)' % (
-                    table, col_names, wildcards)
-                data = [tuple(x) for x in frame.values]
-                print insert_query
-                print data
-                cur.executemany(insert_query, data)
-
-            _write_postgresql(
-                to_insert,
-                'daily_prices_temp_tbl',
-                [s.replace(' ', '_').strip() for s in to_insert.columns],
-                cursor
-            )
-
-        except (Exception) as error :
-            print('PySparkError: ' + str(error))
-
-    rdd.foreach(rddProcess)
+    rdd.foreachPartition(rddProcess)
 
 directKafkaStream.foreachRDD(processStream)
 
