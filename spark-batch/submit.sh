91bc366b8170f7034b1f9d44275b6a050827f1a0
matthewlamdotjs <matthewlam.js@gmail.com>
2020-02-07 15:54:55 -0800

    remove yarn syntax

--- spark-batch/submit.sh
+++ spark-batch/submit.sh
@@ -3,7 +3,7 @@
 /usr/local/spark/bin/spark-submit \
   --driver-class-path /usr/local/postgresql-42.2.9.jar \
   --master spark://ec2-44-232-120-145.us-west-2.compute.amazonaws.com:7077 \
-  --num-executors 3 \
+  --conf "spark.cores.max=12" --conf "spark.executor.cores=4" \
   --driver-memory 4g \
   --executor-memory 2g \
   --executor-cores 4 \
--- spark-stream/submit.sh
+++ spark-stream/submit.sh
@@ -4,7 +4,7 @@
   --driver-class-path /usr/local/postgresql-42.2.9.jar \
   --jars ~/spark-streaming-kafka-0-8-assembly_2.11-2.4.0.jar \
   --master spark://ec2-44-232-120-145.us-west-2.compute.amazonaws.com:7077 \
-  --num-executors 3 \
+  --conf "spark.cores.max=12" --conf "spark.executor.cores=4" \
   --driver-memory 4g \
   --executor-memory 2g \
   --executor-cores 4 \
