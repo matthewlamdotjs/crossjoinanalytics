a955a1fce6a07892170805258a917769ea178f62
matthewlamdotjs <matthewlam.js@gmail.com>
2020-02-07 18:25:55 -0800

    remove try-catch

--- spark-stream/stream-consumer.py
+++ spark-stream/stream-consumer.py
@@ -107,78 +107,79 @@ def processStream(time, rdd):
 
             symbol = 'error'
 
-            try:
-                # parse json
-                js_payload = json.loads(response)
-                symbol = js_payload['Meta Data']['2. Symbol']
-                ts = js_payload['Time Series (Daily)']
+            # try:
+            # parse json
+            js_payload = json.loads(response)
+            symbol = js_payload['Meta Data']['2. Symbol']
+            ts = js_payload['Time Series (Daily)']
 
-                print('parsed_json')
+            print('parsed_json')
 
-                # aggregate response into a dataframe-convertable format
-                def normalize(row):
-                    nested = row[1]
-                    return [symbol, row[0], nested['2. high'], nested['3. low'], nested['1. open'], nested['4. close']]
-                
-                thelist = list(
-                    map(normalize, list(map(list, ts.items())))
-                )
-
-
-                print('made list')
-
-                # Get symbol currency
-                cursor.execute(
-                    'SELECT currency FROM symbol_master_tbl where symbol = \''+symbol+'\';'
-                )
-
-                print('execd_cursor')
-
-                currency = cursor.fetchone()[0]
-
-                print('currency: '+currency)
-                
-                # get exisiting data
-                datesDF = psql.read_sql("""
-                    SELECT
-                        date
-                    FROM
-                        daily_prices_temp_tbl
-                    WHERE
-                        symbol = '"""+symbol+"""';
-                """, connection)
+            # aggregate response into a dataframe-convertable format
+            def normalize(row):
+                nested = row[1]
+                return [symbol, row[0], nested['2. high'], nested['3. low'], nested['1. open'], nested['4. close']]
+            
+            thelist = list(
+                map(normalize, list(map(list, ts.items())))
+            )
 
 
-                print('dates')
-                print(datesDF.head())
+            print('made list')
 
-                # make DF from new data
-                newDF = pd.DataFrame(thelist, columns =['symbol','date','price_high','price_low','price_open','price_close'])
+            # Get symbol currency
+            cursor.execute(
+                'SELECT currency FROM symbol_master_tbl where symbol = \''+symbol+'\';'
+            )
 
-                # subtract old data
-                key_diff = set(newDF.date).difference(datesDF.date)
-                where_diff = newDF.date.isin(key_diff)
+            print('execd_cursor')
 
-                to_insert = newDF[where_diff]
-                to_insert['price_usd'] = to_insert['price_close'].apply(convert_usd)
+            currency = cursor.fetchone()[0]
 
-                print(currency)
-                print(to_insert.head())
+            print('currency: '+currency)
+            
+            # get exisiting data
+            datesDF = psql.read_sql("""
+                SELECT
+                    date
+                FROM
+                    daily_prices_temp_tbl
+                WHERE
+                    symbol = '"""+symbol+"""';
+            """, connection)
 
-                # _write_postgresql(
-                #     to_insert,
-                #     'daily_prices_temp_tbl',
-                #     [s.replace(' ', '_').strip() for s in to_insert.columns],
-                #     cursor
-                # )
 
-            except (Exception) as error :
-                print('PySparkError: ' + str(error))
-                return (symbol, 0)
+            print('dates')
+            print(datesDF.head())
+
+            # make DF from new data
+            newDF = pd.DataFrame(thelist, columns =['symbol','date','price_high','price_low','price_open','price_close'])
+
+            # subtract old data
+            key_diff = set(newDF.date).difference(datesDF.date)
+            where_diff = newDF.date.isin(key_diff)
+
+            to_insert = newDF[where_diff]
+            to_insert['price_usd'] = to_insert['price_close'].apply(convert_usd)
+
+            print(currency)
+            print(to_insert.head())
+
+            # _write_postgresql(
+            #     to_insert,
+            #     'daily_prices_temp_tbl',
+            #     [s.replace(' ', '_').strip() for s in to_insert.columns],
+            #     cursor
+            # )
+
+            # except (Exception) as error :
+            #     print('PySparkError: ' + str(error))
+            #     return (symbol, 0)
 
             return (symbol, 1)
 
-        return partition.foreach(processMessage)
+        for message in partition:
+            processMessage(message)
 
     print(rdd.mapPartitions(rddProcess).collect())
 
