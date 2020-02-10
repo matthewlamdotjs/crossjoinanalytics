5be086518ee61cf6f8949038d8c8b15e3b1e9ad9
matthewlamdotjs <matthewlam.js@gmail.com>
2020-02-09 20:22:39 -0800

    balance cores

--- spark-batch/submit.sh
+++ spark-batch/submit.sh
@@ -3,10 +3,10 @@
 /usr/local/spark/bin/spark-submit \
   --driver-class-path /usr/local/postgresql-42.2.9.jar \
   --master spark://ec2-44-232-120-145.us-west-2.compute.amazonaws.com:7077 \
-  --conf "spark.cores.max=12" --conf "spark.executor.cores=4" \
-  --driver-memory 4g \
-  --executor-memory 2g \
-  --executor-cores 4 \
+  --conf "spark.cores.max=9" --conf "spark.executor.cores=3" \
+  --driver-memory 6g \
+  --executor-memory 3g \
+  --executor-cores 3 \
   ~/crossjoinanalytics/spark-batch/volatility_aggregation.py \
   1000
   
--- spark-stream/submit.sh
+++ spark-stream/submit.sh
@@ -4,10 +4,9 @@
   --driver-class-path /usr/local/postgresql-42.2.9.jar \
   --jars ~/spark-streaming-kafka-0-8-assembly_2.11-2.4.0.jar \
   --master spark://ec2-44-232-120-145.us-west-2.compute.amazonaws.com:7077 \
-  --conf "spark.cores.max=12" --conf "spark.executor.cores=4" \
-  --driver-memory 4g \
-  --executor-memory 2g \
-  --executor-cores 4 \
+  --conf "spark.cores.max=9" --conf "spark.executor.cores=3" \
+  --driver-memory 6g \
+  --executor-memory 3g \
+  --executor-cores 3 \
   ~/crossjoinanalytics/spark-stream/stream-consumer.py \
   1000
-  
