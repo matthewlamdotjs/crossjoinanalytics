a1506e3666a6aef5766950f81525c798f0aa580c
matthewlamdotjs <matthewlam.js@gmail.com>
2020-02-11 13:18:24 -0800

    custom log file for kafka producer

--- kafka-q/bg_submit.sh
+++ kafka-q/bg_submit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # submit as background task
 
-nohup python ~/crossjoinanalytics/kafka-q/producer.py &
+nohup python ~/crossjoinanalytics/kafka-q/producer.py > ~/k_daily_producer_log.out 2>&1
